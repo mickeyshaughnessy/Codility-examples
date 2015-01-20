924cfd0a2aac4ff33e281a5e70d817a04d9d6901
Michael Shaughnessy <mickeyshaughnessy@gmail.com>
2015-01-19 18:00:25 -0800

    working version of FrogRiverOne.py, in O(N) time

--- FrogRiverOne.py
+++ FrogRiverOne.py
@@ -1,11 +1,13 @@
 def solution(X, A):
-    required = list(xrange(X)) 
-    
+    counts = [0] * (X + 1)
     time = 0
-    for leaf in A:
-        if leaf - 1 in required:
-            required.remove(leaf-1)
-        if len(required) == 0:
+    euler = 0
+    for i in A:
+        if i <= X:
+            if counts[i] == 0: 
+                euler += i
+            counts[i] += 1
+        if euler == X * (X + 1) / 2:
             return time
         time += 1
     return -1
